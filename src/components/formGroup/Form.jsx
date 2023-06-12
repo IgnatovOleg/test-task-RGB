502e4066a60d0c785270477b99dac25448b28e06
IgnatovOleg <08623.ignatovoleg@gmail.com>
2023-06-12 22:19:20 +0300

    added sending data to email

--- src/components/formGroup/Form.jsx
+++ src/components/formGroup/Form.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useRef} from "react";
 import Input from "./form/Input";
 import "./Form.scss"
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import emailjs from "emailjs-com";
 
 
 
@@ -20,8 +21,16 @@ const Form = () => {
         resolver: yupResolver(schema),
     });
 
-    const onSubmit = (data) => {
-        alert(JSON.stringify(data))
+    const form = useRef();
+
+    const onSubmit = () => {
+
+        emailjs.sendForm('service_crrv3v9', 'template_rqavohp', form.current, 'NYb4hLJM135sTvLCD')
+            .then((result) => {
+                alert("Сообщение отправлено")
+            }, (error) => {
+                alert("Ошибка")
+            });
     }
 
 
@@ -29,7 +38,7 @@ const Form = () => {
         <div className="form-group-container">
             <h1>Запишитесь <p>бесплатно</p>
                 и получите подарок</h1>
-            <form onSubmit={handleSubmit(onSubmit)}>
+            <form ref={form} onSubmit={handleSubmit(onSubmit)}>
                 <Input
                     register={register("userName", {
                         required: true,
@@ -37,17 +46,24 @@ const Form = () => {
                     placeholder="Ваше имя и фамилия"
                 />
                 <Input
+                    countryList={"countryList"}
                     register={register("phone", {
-                        required: true,
+                        required: true
                     })}
                     placeholder="Ваш номер телефона" />
+                <datalist id="countryList">
+                    <option value="+52 5">usa</option>
+                    <option value="+1 437">Canada</option>
+                    <option value="+380">UK</option>
+                    <option value="8 10 612">Australia</option>
+                </datalist>
                 <Input
                     register={register("email", {
                         required: true
                     })}
                     placeholder="Ваш email"
                 />
-                <button type="submit">Записаться бесплатно</button>
+                <button>Записаться бесплатно</button>
             </form>
             <div className="confidentiality">
                 <span>Нажимая на кнопку я соглашаюсь <p>c политикой конфиденциальности</p></span>
--- src/components/formGroup/form/Input.jsx
+++ src/components/formGroup/form/Input.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import "./Input.scss"
 
 
-const Input = ({placeholder, register}) => {
+const Input = ({placeholder, register, countryList}) => {
 
 
 
     return(
         <div className="form-container">
-            <input type="text" placeholder={placeholder} {...register}/>
+            <input type="text" placeholder={placeholder} {...register} list={countryList}/>
         </div>
     )
 }
